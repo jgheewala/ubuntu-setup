ea809275cf20dbeff4160a733fe02846af889832
Jay Gheewala <jay.a.gheewala@gmail.com>
2018-04-01 17:36:09 -0700

    added code to install go1.10

--- go-setup.sh
+++ go-setup.sh
@@ -1,13 +1,3 @@
-sudo add-apt-repository -y ppa:ubuntu-lxc/lxd-stable
-sudo apt-get update
-sudo apt-get -y install golang
-
-export GOPATH=$HOME/.go
-export PATH=$HOME/.go/bin:$PATH
-cat <<'EOS' >> ~/.bashrc
-export GOPATH=$HOME/.go
-export PATH=$HOME/.go/bin:$PATH
-EOS
-
-# ref. https://github.com/peco/peco#go-get
-go get github.com/peco/peco/cmd/peco
+cd /tmp
+wget https://dl.google.com/go/go1.10.1.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.10.1.linux-amd64.tar.gz
